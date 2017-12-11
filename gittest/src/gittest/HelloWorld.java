6ba11e86c90bf551ad50a33bd30f650fd25bf6cc
seuncho <topdns2998@gmail.com>
2017-12-11 15:26:17 +0900

    출력문 추가

--- gittest/src/gittest/HelloWorld.java
+++ gittest/src/gittest/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!!!!!");
 		System.out.println("안녕하세요");
+		System.out.println("마스터 작업중");
 
 	}
 
